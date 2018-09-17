ebda1fd71061ccb628e8d58c72e31e4e966db4ac
Jess Cabradilla <jess.cabradilla@gmail.com>
2018-09-18 01:55:56 +0800

    referenced scss file

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 const TextBox = () => {
   return (
     <div>
--- client/js/index.js
+++ client/js/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import Main from "./App.jsx"
 
+import "../scss/styles.scss";
+
 ReactDOM.render(<Main />, document.querySelector("main"));
